aa29124087a5dce69a822bcbeacb8eaab752ffa7
tommiebupt <taoriming10@163.com>
2018-12-29 14:59:29 +0800

    modified

--- eval_graph.py
+++ eval_graph.py
@@ -32,11 +32,11 @@ class EvalGraph(object):
 def test():
   from gender_data import GenderEvalData 
   os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
-  g_eval_data = GenderEvalData(num_classes=2, label_info={'male':0, 'female':1})
-  g_eval_data.load_data("../../data/jd_image/data/1315_sex_test_samples.dat", 
-      "../../data/jd_image/test")
+  g_eval_data = GenderEvalData(num_classes=6)
+  g_eval_data.load_data("test_samples.dat", 
+      "images/test")
 
-  _eval_op, _final_op, _variables = EvalGraph(g_eval_data, 256, 256).get_eval_ops()  
+  _eval_op, _final_op, _variables = EvalGraph(g_eval_data, 256).get_eval_ops()  
   with tf.Session() as sess:
     init_op = tf.group([tf.global_variables_initializer(),
         tf.local_variables_initializer(), tf.tables_initializer()])
